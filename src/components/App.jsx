37a9acdb1e2bb8c3ff90db19c7cbe6813994c5a7
PabloG <pawelgarbula@gmail.com>
2024-07-11 12:49:59 +0200

    first initial

---
+++ src/components/App.jsx
@@ -0,0 +1,27 @@
+import Profile from "../components/Profile/Profile";
+import Statistics from "../components/Statistics/Statistics";
+import FriendList from "../components/FriendList/FriendList";
+import Transactions from "../components/Transactions/Transactions";
+import stastics from "../data/statistics.json";
+import user from "../data/user.json";
+import friends from "../data/friends.json";
+import transactions from "../data/transactions.json";
+
+function App() {
+  return (
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={stastics} />
+      <FriendList friends={friends} />
+      <Transactions transactions={transactions} />
+    </>
+  );
+}
+
+export default App;
